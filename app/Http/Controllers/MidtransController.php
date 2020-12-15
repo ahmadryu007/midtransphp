0446698dc6a29e6caa15d558ba1722c9bef94175
ahmad krisman ryuzaki <ahmadryuzaki007@icloud.com>
2020-12-15 17:33:36 +0700

    charge function

--- app/Http/Controllers/MidtransController.php
+++ app/Http/Controllers/MidtransController.php
@@ -21,8 +21,23 @@ class MidtransController extends Controller
     //
 
     public function charge(Request $request){
-        Log::info('Midtrans Charge Start ====== ');
         Log::info('Midtrans Request : '.json_encode($request->all()));
+
+        $client = new \GuzzleHttp\Client();
+        $response = $client->request('GET', 'https://api.github.com/repos/guzzle/guzzle');
+
+        echo $response->getStatusCode(); // 200
+        echo $response->getHeaderLine('content-type'); // 'application/json; charset=utf8'
+        echo $response->getBody(); // '{"id": 1420053, "name": "guzzle", ...}'
+
+        // Send an asynchronous request.
+        $request = new \GuzzleHttp\Psr7\Request('GET', 'http://httpbin.org');
+        $promise = $client->sendAsync($request)->then(function ($response) {
+            echo 'I completed! ' . $response->getBody();
+        });
+
+        $promise->wait();
+
         return "tokenResponse";
     }
 
